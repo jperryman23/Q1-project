6cc00f454d2d1e0daf52008e7e20db09ca352794
Jules Perryman <jules.perryman@gmail.com>
2017-03-16 03:02:31 -0600

    Almost done

--- newtab.js
+++ newtab.js
@@ -6,23 +6,31 @@ $(document).ready(function() {
 
     $(".toDoList").draggable();
     $(".weather").draggable();
-    $(".noName").draggable();
+    $(".noNews").draggable();
+    $(".notes").draggable();
 
     $(".resizable").resizable();
 
     $("ul").sortable();
 
-
+    $('#textarea1').val('New Text');
+    $('#textarea1').trigger('autoresize');
 
         var time1 = moment().utcOffset('dddd, MMMM Do YYYY, HH:MM:ss');
-        console.log(time1);
+        // console.log(time1);
         $(".time").append(time1._d)
 
-
+        $("#test1").click(function(){
+            $("label").toggleClass("cssStyle"); })
 
         // var time2 = moment().utcOffset('dddd MMMM Do YYYY HH MM SS-5');
         // console.log(time2._d);
         // $(".time.2").append(time2._d)
+
+        var now = new Date(Date.now());
+        var formatted = now.getDate() + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+        console.log(formatted);
+        $("h4").append("Clock: " + formatted);
 });
 
 
@@ -38,16 +46,16 @@ $(".buttonAdd").on('click', function(){
 
 
 
-$(function(){
-  $(".checkbox").change(function(){
-    if ($(this).is(':checked')) {
-        $("label").css("text-decoration", "line-through")
-    //    alert('You can rock now...');
-} else {
-    $("label").css("text-decoration", " ");
-};
-  });
-});
+// $(function(){
+//   $(".checkbox").change(function(){
+//     if ($(this).is(':checked')) {
+//         $("label").css("text-decoration", "line-through")
+//     //    alert('You can rock now...');
+// } else {
+//     $("label").css("text-decoration", " ");
+// };
+//   });
+// });
 
 
 
@@ -88,9 +96,70 @@ $.ajax({
             alert('An error has occurred');
         },
         success: function(data){
-            //   console.log(data);
+            //    console.log(data);
               $(".city").append(data.current_observation.display_location.full);
               $(".temp").append("Temperature: " + data.current_observation.temperature_string);
               $(".icon").append("<img src='" + data.current_observation.icon_url + "'>");
             }
-    })
+    });
+
+    $.ajax({
+            url: 'http://api.wunderground.com/api/59d1d82f8ef55eeb/forecast10day/q/CO/Denver.json',
+            type: 'GET',
+            data: {
+                format: 'json'
+            },
+            error: function(){
+                alert('An error has occurred');
+            },
+            success: function(data){
+                //   console.log(data);
+                  $(".conditionsToday").append(data.forecast.simpleforecast.forecastday["0"].conditions);
+                  $(".conditionsTomorrow").append(data.forecast.txt_forecast.forecastday[2].fcttext);
+                  $(".weekday.nextDay").append(data.forecast.txt_forecast.forecastday[2].title);
+                  $(".iconTomorrow").append("<img src='" + data.forecast.txt_forecast.forecastday[2].icon_url + "'>");
+
+                  $(".conditionsPlusTwo").append(data.forecast.txt_forecast.forecastday[4].fcttext);
+                  $(".weekday.plusTwo").append(data.forecast.txt_forecast.forecastday[4].title);
+                  $(".iconPlusTwo").append("<img src='" + data.forecast.txt_forecast.forecastday[4].icon_url + "'>");
+
+                //   $(".temp").append("Temperature: " + data.current_observation.temperature_string);
+                //   $(".icon").append("<img src='" + data.current_observation.icon_url + "'>");
+                }
+        })
+
+        $.ajax({
+                url: 'https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=3286922d938b4e6bb9e32cb1b3e3b57e',
+                type: 'GET',
+                data: {
+                    format: 'json'
+                },
+                error: function(){
+                    alert('An error has occurred');
+                },
+                success: function(data){
+                    //   console.log(data.articles["0"]);
+
+                    $(".newsImage").append(
+                      "<img src='" + data.articles["0"].urlToImage + "' height='75px' width='75px'>")
+                    //  $(".col.s9.test").append(data.articles["0"].title),
+                    $(".newDesc").append(data.articles["0"].description)
+
+
+                    $(".newsImage2").append(
+                      "<img src='" + data.articles["1"].urlToImage + "' height='75px' width='75px'>")
+                    //  $(".col.s9.test").append(data.articles["0"].title),
+                    $(".newDesc2").append(data.articles["1"].description)
+
+                    $(".newsImage3").append(
+                      "<img src='" + data.articles["2"].urlToImage + "' height='75px' width='75px'>")
+                    //  $(".col.s9.test").append(data.articles["0"].title),
+                    $(".newDesc3").append(data.articles["2"].description)
+
+                    $(".newsImage4").append(
+                      "<img src='" + data.articles["3"].urlToImage + "' height='75px' width='75px'>")
+                    //  $(".col.s9.test").append(data.articles["0"].title),
+                    $(".newDesc4").append(data.articles["3"].description)
+
+                    }
+            })
